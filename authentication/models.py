2f68dd97025a0e5aa7d40f4263de63acabf29b75
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2020-07-31 13:15:34 +0530

    Created Inital Data Migrations

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Role(models.Model):
     role = models.CharField(max_length=10, blank=False, null=False)
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField('User', on_delete=models.CASCADE, blank=False, null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False)
     role = models.OneToOneField(Role, on_delete=models.CASCADE, blank=False, null=False)
--- mlm/settings.py
+++ mlm/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
     'rest_framework',
     'corsheaders',    
+    # custom
+    'authentication',
+    'commissions'
 ]
 
 MIDDLEWARE = [
