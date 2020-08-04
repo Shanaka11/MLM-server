30eef405c3ac405adc0518244fed09c06c10e54b
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2020-08-04 23:56:30 +0530

    Added Comments

--- commissions/api.py
+++ commissions/api.py
@@ -6,6 +6,7 @@ from .models import Sales, Salesperson
 class SalespersonApi(viewsets.ModelViewSet):
     queryset = Salesperson.objects.all()
     serializer_class = SalespersonSeralizer
+    # When Creating Salesperson create a user as well if password is not given set a default password
 
 class SalesApi(viewsets.ModelViewSet):
     queryset= Sales.objects.all()
