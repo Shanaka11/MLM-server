83c9346a54144d35bc882f3a531ba76d295c0df0
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2020-08-11 12:20:12 +0530

    Search Functionality

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import SalesApi, SalespersonApi
-from .views import getConnectedSalesperson
+from .views import getConnectedSalesperson, searchSalesperson, searchSales
 
 router = DefaultRouter()
 router.register('sales', SalesApi)
@@ -9,5 +9,7 @@ router.register('salesperson', SalespersonApi)
 
 urlpatterns = [
     path('',include( router.urls )),
-    path('connected_salesperson/<int:id>', getConnectedSalesperson)
+    path('connected_salesperson/<int:id>', getConnectedSalesperson),
+    path('salesperson/search/<str:name>', searchSalesperson),
+    path('sales/search/<str:name>', searchSales)
 ]
--- commissions/views.py
+++ commissions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Salesperson
-from .serializers import SalespersonSeralizer
+from .models import Salesperson, Sales
+from .serializers import SalespersonSeralizer, SalesSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -10,4 +10,15 @@ def getConnectedSalesperson(request, id):
     salespersons = Salesperson.objects.filter(sponser_id = id)
     serializer = SalespersonSeralizer(salespersons , many=True)
     return Response(serializer.data, status=200)
-    
+
+@api_view(('GET',))
+def searchSalesperson(response, name):
+    salespersons = Salesperson.objects.filter(name__icontains = name)    
+    serializer = SalespersonSeralizer(salespersons , many=True)
+    return Response(serializer.data, status=200)
+
+@api_view(('GET', ))
+def searchSales(response, name):
+    sales = Sales.objects.filter(salesperson__id__icontains = name)
+    serializer = SalesSerializer(sales, many=True)
+    return Response(serializer.data, status=200)
